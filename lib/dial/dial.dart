86f3ed5e70f6f9f06538f70aceffb6ebc0e451df
Paul Crimes <1911935+crimesp@users.noreply.github.com>
2024-12-15 21:36:27 +0000

    dialog - made text size based on font size not available space

--- lib/dial/dial.dart
+++ lib/dial/dial.dart
@@ -304,7 +304,7 @@ class DialState extends State<Dial> with SingleTickerProviderStateMixin {
   List<TextPainter> _buildBaseUnitLabels(TextTheme textTheme, Size size) {
     var fontSizeBase = 12;
     // var fontSizeBase = 0.045;
-    print(' size.shortestSide ${ size.shortestSide}' );
+    // print(' size.shortestSide ${ size.shortestSide}' );
     final style =
         textTheme.titleMedium!.copyWith(fontSize:  fontSizeBase * MediaQuery.of(context).textScaleFactor);
     // fontSize: size.shortestSide * 0.07
